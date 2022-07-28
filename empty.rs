d93c4583f8bfc9c7b173398e31ccc326b1199418
delskayn <mees.delzenne@gmail.com>
2022-07-28 13:57:35 +0200

    Removed unnecessary files

--- empty.rs
+++
@@ -1,2 +0,0 @@
-expec: <for<'r, 'cell, 'gc> fn(&'r mut CellOwner<'cell>, &'gc mut Root<'cell>) -> Vec<Gc<'gc, 'cell, usize>> {init} as FnOnce<(&mut CellOwner<'_>, & mut Root<'_>)>>::Output`
-found: <for<'r, 'cell, 'gc> fn(&'r mut CellOwner<'cell>, &'gc mut Root<'cell>) -> Vec<Gc<'gc, 'cell, usize>> {init} as FnOnce<(&mut CellOwner<'cell> , &'gc mut Root<'cell>)>>::Output`
